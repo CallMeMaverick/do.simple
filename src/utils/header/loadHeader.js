49cc28c1bd1910261e5e510d7525bd17b3688807
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-02-26 15:43:07 +0100

    style: Style header

--- src/utils/header/loadHeader.js
+++ src/utils/header/loadHeader.js
@@ -4,7 +4,10 @@ export default function loadHeader() {
     const header = document.createElement("header");
 
     const headerHeading = document.createElement("h1");
-    headerHeading.textContent = "Do Simple";
+    headerHeading.textContent = "Do.";
+    const headerSpan = document.createElement("span");
+    headerSpan.textContent = "Simple";
+    headerHeading.appendChild(headerSpan);
 
     const headerPara = document.createElement("p");
     headerPara.textContent = "Minimalistic to-do app to manage your daily chores";
