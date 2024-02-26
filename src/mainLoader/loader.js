44335462d5bd069e433172513576ff05698ae62e
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-02-26 16:42:13 +0100

    feat: Add addButtonLoader to Loader class

--- src/mainLoader/loader.js
+++ src/mainLoader/loader.js
@@ -1,7 +1,9 @@
 import loadHeader from "../utils/header/loadHeader";
+import loadAddButton from "../utils/addButton/addButtonLoader";
 
 export default class Loader {
     static load() {
         document.body.appendChild(loadHeader());
+        document.body.appendChild(loadAddButton());
     }
 }
