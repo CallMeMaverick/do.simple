2ef55cafd4b989030de6d3189bc5a6b24e6c4489
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-03-02 15:01:29 +0100

    chore: taskWindowLoader extended

--- src/taskWindowLoader/taskWindowLoader.js
+++ src/taskWindowLoader/taskWindowLoader.js
@@ -1,14 +1,13 @@
 import addTaskButton from "../objects/task/addTaskButton/addTaskButton";
 import addTable from "../objects/task/addTable/addTable";
+import createModuleTask from "../objects/task/moduleTask/createModuleTask";
 
 export default function taskWindowLoader(project) {
     document.body.innerHTML = "";
 
-    // Extract the project's title and description from the provided 'project' element
     const projectTitle = project.querySelector(".title").textContent;
     const projectDescription = project.querySelector(".desc").nextSibling.textContent.trim();
 
-    // Create the header elements
     const header = document.createElement("header");
     const headerHeading = document.createElement("h1");
     headerHeading.textContent = projectTitle;
@@ -16,15 +15,13 @@ export default function taskWindowLoader(project) {
     const headerPara = document.createElement("p");
     headerPara.textContent = projectDescription;
 
-    // Append the header elements to the header
     header.appendChild(headerHeading);
     header.appendChild(headerPara);
 
-    // Append the header to the document body
     document.body.appendChild(header);
 
-    // Optionally append other elements if needed
     document.body.appendChild(addTaskButton());
     document.body.appendChild(addTable());
+    document.body.appendChild(createModuleTask());
 }
 
